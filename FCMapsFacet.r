db351f5a6ef6cf42c29567f427d0fbb95b8cc5e5
frank <fedwards@uw.edu>
2016-01-27 13:19:43 -0800

    update for pov / ent map

--- FCMapsFacet.r
+++ FCMapsFacet.r
@@ -90,6 +90,46 @@ print(MapPlot)
 
 ggsave(plot = MapPlot, "FacetMap.png", h = 8, w = 8)
 
+### FOR 1X2 ENTRY/POV MAPS FOR TALK
+
+n<-nrow(fc10)
+fclong<-with(fc10, data.frame(region=rep(tolower(fc10$statename), 2),
+                              q=as.factor(c(returnquant(entrt), returnquant(chpov))),
+                              c=c(rep("Foster Care Entries per Child Population", n), rep("Child Poverty Rate", n))
+))
+fclong$c<-factor(fclong$c, levels=c("Foster Care Entries per Child Population", 
+                                    "Child Poverty Rate"))
+choro<-merge(states, fclong, by="region")
+choro <- choro[order(choro$order), ]
+
+MapPlot <- ggplot(choro,
+                  aes(x = long, y = lat, group = group, fill = q))
+MapPlot <- MapPlot + geom_polygon(aes(fill = q), colour = "gray20", size = 0.1) +
+  scale_fill_brewer(palette = "Blues",
+                    name="Average\nState Value\n2002-2011", labels=c("Lowest 20%", " ", " ", " ", "Highest 20%"))
+
+MapPlot <- MapPlot + coord_map(project="albers", at0 = 45.5, lat1 = 29.5)  # Changes the projection to something other than Mercator.
+
+MapPlot <- MapPlot +  theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank(),
+                            panel.border = element_blank(), panel.background=element_blank())+
+  scale_y_continuous(name="", breaks=NULL)+
+  scale_x_continuous(name="", breaks=NULL)+
+  theme(legend.title=element_text(size=10))+
+  theme(legend.text=element_text(size=10))+
+  theme(legend.position="bottom")+
+  theme(legend.key.size= unit(0.3, "cm"))
+
+MapPlot<- MapPlot + theme(strip.background=element_blank(), 
+                          strip.text.x=element_text(size=10),
+                          strip.text.y=element_blank())
+
+MapPlot <- MapPlot + xlab(NULL) + ylab(NULL)
+
+MapPlot <- MapPlot + facet_wrap(~c, ncol=2)
+
+print(MapPlot)
+
+ggsave(plot = MapPlot, "PovEntMap.pdf", h = 6, w = 8)
 
 
 
